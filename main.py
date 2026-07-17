0e28a56a8ccf43bff8ae6061877ca875d1d70496
eltails9000-cyber <eltails9000@gmail.com>
2026-07-16 20:34:24 -0500

    Update Config class to use placeholder values
    Replaced environment variable retrieval with placeholders.

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import os
-
 class Config:
-    TOKEN = os.getenv("DISCORD_TOKEN")
-
-    API_URL = os.getenv("API_URL")
-    API_KEY = os.getenv("ROBLOX_API_KEY")
-
+    TOKEN = ...
+    API_URL = ...
+    API_KEY = ...
     OWNER_ID = 1042929958286266540
-
 
 config = Config()
